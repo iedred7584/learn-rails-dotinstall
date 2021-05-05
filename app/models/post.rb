dc29abe563077c55f1a7f9de59fa9b24f5df3358
iedred7584 <me@iedred7584.com>
2021-05-05 15:25:12 +0900

    :sparkles: add comment auto delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3, maximum: 20, message: "length error !!" }
   validates :body, presence: true

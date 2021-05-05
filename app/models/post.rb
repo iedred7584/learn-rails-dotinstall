d56d7b4118e1d995ced039ccbd424cfaeed47b13
iedred7584 <me@iedred7584.com>
2021-05-05 12:44:37 +0900

    :sparkles: add comment model & controller & routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 3, maximum: 20, message: "length error !!" }
   validates :body, presence: true
 end

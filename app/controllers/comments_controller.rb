b13783bc8d59453c9555dfc8b8f52fc8b00f8a4e
iedred7584 <me@iedred7584.com>
2021-05-05 13:35:08 +0900

    :sparkles: add comment save func

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.save
+    redirect_to post_path(@post.id)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end

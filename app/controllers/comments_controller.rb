4ebb685d343d9e207daae0517611ee591f69635a
iedred7584 <me@iedred7584.com>
2021-05-05 15:54:39 +0900

    :sparkles: add comment edit page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,21 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post.id)
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post.id)
+    else
+      redirect_to edit_post_comment_path(@post.id, @comment.id)
+    end
+  end
+
   private
 
   def comment_params
